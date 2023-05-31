79a3401be0aa78ebaa50018f52388aa98b1e163e
JoFeG <fego.jose@gmail.com>
2023-05-31 12:09:30 -0400

    series matrix constructor

--- dimerc/src/utils.jl
+++ dimerc/src/utils.jl
@@ -1,4 +1,4 @@
-function jsonpoints2array(points::JSON3.Array,include_time=false)
+function jsonpoints2arrays(points::JSON3.Array,include_time=false)
     m = length(points)    
     
     x = [points[k].x for k in 1:m]
@@ -12,3 +12,35 @@ function jsonpoints2array(points::JSON3.Array,include_time=false)
     return x, y
 end
 
+function seriesjson2matrix(points::JSON3.Array)
+    n = length(json)
+    series = []
+    xs = []
+    for k = 1:n
+        x, y = jsonpoints2arrays(json[k].points)
+
+        series = [series..., y]
+        xs = [xs..., x]
+    end
+    max_x = maximum(maximum.(xs))
+    min_x = minimum(minimum.(xs))
+    println("min_x = ", min_x)
+    println("max_x = ", max_x)
+    m = max_x - min_x
+
+    Y = zeros(m,n)
+
+    for k = 1:n
+        for i = 1:m
+            #println("k=",k,", i=",i)
+            data = series[k][xs[k].==min_x+i-1]
+            if length(data)>0
+                Y[i,k] = data[1]
+            else
+                Y[i,k] = 0
+            end
+        end
+    end
+    
+    return Y, min_x
+end
